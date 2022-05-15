4a43fa76b42d90dac05c3913566bd11b64059aec
Nick Fisher <github@nickdafish.com>
2022-05-15 15:09:23 -0400

    another try

---
+++ sprites.background.ts
@@ -0,0 +1,8 @@
+namespace sprites.background {
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const u9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const u92 = image.ofBuffer(hex``);
+}
