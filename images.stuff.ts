5f834ade22e4532dfff0fdf1e01403ad4e35d3a7
Nick Fisher <github@nickdafish.com>
2022-05-15 15:18:15 -0400

    try again

---
+++ images.stuff.ts
@@ -0,0 +1,8 @@
+namespace images.stuff {
+    //% fixedInstance jres blockIdentity=images.__imagePicker
+    //% tags="background"
+    export const u9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images.__imagePicker
+    //% tags="background"
+    export const u92 = image.ofBuffer(hex``);
+}
